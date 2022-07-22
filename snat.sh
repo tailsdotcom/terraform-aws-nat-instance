780e6a065c189ac3d8699867c5cde58ab96c5e91
Sam Weston <sam.weston@tails.com>
2022-07-22 18:39:33 +0100

    Try and resolve issue of not using EIP

--- snat.sh
+++ snat.sh
@@ -14,7 +14,11 @@ sysctl -q -w net.ipv4.conf.default.rp_filter=0
 # enable IP forwarding and NAT
 sysctl -q -w net.ipv4.ip_forward=1
 sysctl -q -w net.ipv4.conf.eth1.send_redirects=0
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+
+# switch the default route to eth1
+ip route del default dev eth0
 
 # wait for network connection
 curl --retry 10 http://www.google.com
