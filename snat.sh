351bc174c3f502b09c8d49844f52df840a62bf73
Sam Weston <187511418+samweston-tails@users.noreply.github.com>
2024-12-11 17:17:56 +0000

    More script tweaks

--- snat.sh
+++ snat.sh
@@ -11,18 +11,16 @@ done
 echo "Devices: public: $ETH1 and private: $ETH0"
 
 ### IP forwarding configuration
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.d/90-nat.conf
+echo "net.ipv4.ip_forward=1" > /etc/sysctl.d/90-nat.conf
 echo "net.ipv4.conf.all.rp_filter=0" >> /etc/sysctl.d/90-nat.conf
 echo "net.ipv4.conf.${ETH0}.rp_filter=0" >> /etc/sysctl.d/90-nat.conf
 echo "net.ipv4.conf.${ETH1}.rp_filter=0" >> /etc/sysctl.d/90-nat.conf
 echo "net.ipv4.conf.default.rp_filter=0" >> /etc/sysctl.d/90-nat.conf
-echo "sysctl -q -w net.ipv4.conf.${ETH1}.send_redirects=0" >> /etc/sysctl.d/90-nat.conf
+echo "net.ipv4.conf.${ETH1}.send_redirects=0" >> /etc/sysctl.d/90-nat.conf
 sysctl --system
 
 ### IPTables configuration
-iptables -t nat -A POSTROUTING -o $ETH0 -j MASQUERADE
-iptables -A FORWARD -i $ETH0 -o $ETH1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i $ETH1 -o $ETH0 -j ACCEPT
+iptables -t nat -A POSTROUTING -o $ETH1 -j MASQUERADE
 systemctl enable iptables
 service iptables save
 
