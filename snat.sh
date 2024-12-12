b3c5f2d70ae258960c356859140953a52f4590f5
Sam Weston <187511418+samweston-tails@users.noreply.github.com>
2024-12-12 10:43:24 +0000

    Rework to work for more than 30 minutes on Amazon Linux 2023 (#4)
    * Lift script from ppabis
    
    * Try and rework script again
    
    * More script tweaks
    
    * Use networkd to switch default route priority
    
    * Tweak how we set up networkd
    
    ---------
    
    Co-authored-by: Sam Weston <11150054+cablespaghetti@users.noreply.github.com>

--- snat.sh
+++ snat.sh
@@ -1,27 +1,46 @@
 #!/bin/bash -x
 
-# wait for ens6
-while ! ip link show dev ens6; do
+### Credit to https://github.com/ppabis/nat-instance for script inspiration
+
+# Wait for second interface to appear
+ETH0="ens5"
+ETH1="ens6"
+while ! ip link show dev $ETH1; do
   sleep 1
 done
+echo "Devices: public: $ETH1 and private: $ETH0"
 
-sysctl -q -w net.ipv4.conf.all.rp_filter=0
-sysctl -q -w net.ipv4.conf.ens5.rp_filter=0
-sysctl -q -w net.ipv4.conf.ens6.rp_filter=0
-sysctl -q -w net.ipv4.conf.default.rp_filter=0
+### IP forwarding configuration
+echo "net.ipv4.ip_forward=1" > /etc/sysctl.d/90-nat.conf
+echo "net.ipv4.conf.all.rp_filter=0" >> /etc/sysctl.d/90-nat.conf
+echo "net.ipv4.conf.${ETH0}.rp_filter=0" >> /etc/sysctl.d/90-nat.conf
+echo "net.ipv4.conf.${ETH1}.rp_filter=0" >> /etc/sysctl.d/90-nat.conf
+echo "net.ipv4.conf.default.rp_filter=0" >> /etc/sysctl.d/90-nat.conf
+echo "net.ipv4.conf.${ETH1}.send_redirects=0" >> /etc/sysctl.d/90-nat.conf
+sysctl --system
 
+### IPTables configuration
+iptables -t nat -A POSTROUTING -o $ETH1 -j MASQUERADE
+systemctl enable iptables
+service iptables save
 
-# enable IP forwarding and NAT
-sysctl -q -w net.ipv4.ip_forward=1
-sysctl -q -w net.ipv4.conf.ens6.send_redirects=0
+# Switch the default route to ETH1
+mkdir -p /etc/systemd/network/70-${ETH1}.network.d
 
-iptables -t nat -A POSTROUTING -o ens6 -j MASQUERADE
+cat > /etc/systemd/network/70-${ETH1}.network.d/routepriority.conf << EOF
+[Match]
+Name=ens6
 
-# switch the default route to ens6
-ip route del default dev ens5
+[DHCPv4]
+RouteMetric=500
 
-# wait for network connection
+[IPv6AcceptRA]
+RouteMetric=500
+EOF
+networkctl reload
+
+# Wait for network connection
 curl --retry 10 http://www.google.com
 
-# reestablish connections
+# Reestablish connections
 systemctl restart amazon-ssm-agent.service
